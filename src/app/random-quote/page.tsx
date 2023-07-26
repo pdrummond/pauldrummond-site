06293adccf502db941ac4c67adb7d3948aecf42a
Paul Drummond <paul.drummond@iode.co.uk>
2023-07-26 11:00:10 +0100

    Don't cache the random-quote page to allow browser refresh

--- src/app/random-quote/page.tsx
+++ src/app/random-quote/page.tsx
@@ -2,6 +2,8 @@ import DotLink from "@/components/DotLink";
 import QuoteCard from "@/components/QuoteCard";
 import { getRandomQuote } from "@/data/quotes";
 
+export const revalidate = 0;
+
 export default function QuotePage() {
   return (
     <div>

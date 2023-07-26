36c8583f937f36c08c8c8035effa54f187befef9
Paul Drummond <paul.drummond@iode.co.uk>
2023-07-26 11:37:39 +0100

    random-quote: Added generateStaticParams

--- src/app/random-quote/page.tsx
+++ src/app/random-quote/page.tsx
@@ -1,8 +1,13 @@
 import DotLink from "@/components/DotLink";
 import QuoteCard from "@/components/QuoteCard";
-import { getQuote, getRandomQuoteSlug } from "@/data/quotes";
+import { getAllQuoteSlugs, getQuote, getRandomQuoteSlug } from "@/data/quotes";
 
 export const revalidate = 0;
+
+export async function generateStaticParams() {
+  const quotes = getAllQuoteSlugs();
+  return quotes.map((slug) => ({ slug }));
+}
 
 export default function QuotePage() {
   const slug = getRandomQuoteSlug();

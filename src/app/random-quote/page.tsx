ec354de3342d67b843af2c47aa6b5cffbdaa592b
Paul Drummond <paul.drummond@iode.co.uk>
2023-07-26 11:32:18 +0100

    experimenting with random quote to diagnose bug on vercel

--- src/app/random-quote/page.tsx
+++ src/app/random-quote/page.tsx
@@ -1,14 +1,16 @@
 import DotLink from "@/components/DotLink";
 import QuoteCard from "@/components/QuoteCard";
-import { getRandomQuote } from "@/data/quotes";
+import { getQuote, getRandomQuoteSlug } from "@/data/quotes";
 
 export const revalidate = 0;
 
 export default function QuotePage() {
+  const slug = getRandomQuoteSlug();
+  console.log("Rendering QuotePage with quote:", slug);
   return (
     <div>
       <DotLink />
-      <QuoteCard quote={getRandomQuote()} />
+      <QuoteCard quote={getQuote(slug)} />
     </div>
   );
 }
